a24bf2a61d92fc6c35dc27bab608bab83b9452ea
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-06-20 15:46:48 +0300

    Add hello world file.

---
+++ app.ts
@@ -0,0 +1,10 @@
+
+function hello(person: any){
+    return "Hello, " + person;
+ }
+
+let user = "Vladislav Mihov";
+const result = hello(user);
+console.log("Result", result);
+
+export {}
--- helloworld.ts
+++ helloworld.ts
@@ -3,8 +3,18 @@ function hello(person: any){
     return "Hello, " + person;
  }
 
-let user = "Vladislav Mihov";
-const result = hello(user);
-console.log("Result", result);
+// let user = "Vladislav Mihov";
+// const result = hello(user);
+// console.log("Result", result);
+
+
+// 1. Basic typing
+function basicTyping(){
+    let isDone: boolean = false;
+    let decimal: number = 1.2;
+    console.log(decimal-1.1);
+}
+
+basicTyping();
 
 export {}
