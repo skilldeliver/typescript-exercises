dc2f4c17b7a1b75c61888758ffc034c1cc2fcbb1
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2021-06-20 16:06:21 +0300

    Cover basic typing.

--- helloworld.ts
+++ helloworld.ts
@@ -10,10 +10,42 @@ function hello(person: any){
 
 // 1. Basic typing
 function basicTyping(){
+    // basic
     let isDone: boolean = false;
     let decimal: number = 1.2;
-    console.log(decimal-1.1);
+    
+    let secret: unknown;
+    secret = "Could be anything";
+    secret = 1;
+
+    // array
+    let list: number[] = [1, 2, 3];
+    let list2: Array<number> = [1, 2, 3];
+
+    // tuple
+    let someTuple: [string, number] = ["2", 2];
+
+    // enums
+    enum Color {
+        Red,
+        Green = 2,
+        Blue
+    }
+    let c: Color = Color.Green;
+    console.log(c);
+
+    // type assertion (casting in other languages)
+    let someValue: unknown = "this is a string";
+    let strLength: number = (someValue as string).length;
+    
+    // angle bracket syntax
+    let someValue2: unknown = "this is a string";
+    let strLength2: number = (<string>someValue2).length;
+
+    // let vs var
+    // let is block scoped
 }
+
 
 basicTyping();
 
